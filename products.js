8d184f9cd9f26c74e93bad94e04b3730ce93137e
Dula21 <st20259983@outlook.cardiffmet.ac.uk>
2024-11-25 10:42:18 +0400

    add products.js

---
+++ products.js
@@ -0,0 +1,47 @@
+let product = [
+    {
+        id: 1001,
+                        title: "Dog Food, 25lb Bag",
+                        description: "A 25 pound bag of <em>irresistible</em> organic goodness for your dog",
+                        price: 2000,
+                        image: "image/dog.jpg",
+                        availableInventory: 10,
+                        rating: 3
+    },
+    {
+        id: 1002,
+                        title: "cat Food, 20lb Bag",
+                        description: "A 2 pound bag of <em>irresistible</em> organic goodness for your cat",
+                        price: 2000,
+                        image: "image/cat.jpg",
+                        availableInventory: 10,
+                        rating: 5
+    },
+    {
+        id: 1003,
+                        title: "Dog Food, 15lb Bag",
+                        description: "A 15 pound bag of <em>irresistible</em> organic goodness for your dog",
+                        price: 200,
+                        image: "image/dog.jpg",
+                        availableInventory: 10,
+                        rating: 2
+    },
+    {
+        id: 1004,
+                        title: "Dog Food, 25lb Bag",
+                        description: "A 25 pound bag of <em>irresistible</em> organic goodness for your dog",
+                        price: 2000,
+                        image: "image/dog.jpg",
+                        availableInventory: 10,
+                        rating: 3
+    },
+    {
+        id: 1005,
+                        title: "cat Food, 2l9b Bag",
+                        description: "A 29 pound bag of <em>irresistible</em> organic goodness for your cat",
+                        price: 2000,
+                        image: "image/cats.jpg",
+                        availableInventory: 10,
+                        rating: 1
+    }
+];
